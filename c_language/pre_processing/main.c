a30c3ad256e00daf4d13feac70d7062d26df38e0
Daniel_Lai <a0983121858@gmail.com>
2022-09-06 19:36:11 +0800

    main_code (module_example)

---
+++ c_language/pre_processing/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "module_example.h"
+#include "module_example_funciton.c"
+
+int main(){
+    var1 = 45;
+    var2 = 100;
+    compare_result (MIN_COMPARE);
+    compare_result (MAX_COMPARE);
+}
+
+
