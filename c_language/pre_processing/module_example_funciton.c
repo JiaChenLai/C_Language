b83551f1db0077ca3cae93b81c10b0d7ae02591c
Daniel_Lai <a0983121858@gmail.com>
2022-09-06 19:36:42 +0800

    module_example_function's file

--- c_language/pre_processing/module_example_funciton.c
+++ c_language/pre_processing/module_example_funciton.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 //----'static' means only for this file
-static void max_value(void);
-static void min_value(void);
+//static void max_value(void);
+//static void min_value(void);
 static int result;
 int var1,var2;
 //function code
 static void max_value(){
-    (var1 > var2) ? result = var1 : result = var2;  
+    (var1 > var2) ? (result = var1) : (result = var2);  
 }
 static void min_value(){
-    (var1 < var2) ? result = var1 : result = var2;
+    (var1 < var2) ? (result = var1) : (result = var2);
 }
 
 void compare_result(int num){
