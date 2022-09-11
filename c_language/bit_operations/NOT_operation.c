6781a51845d0092954e2bdf1709180b64a9706ca
Daniel_Lai <a0983121858@gmail.com>
2022-09-11 20:38:31 +0800

    with "&0xff"

--- c_language/bit_operations/NOT_operation.c
+++ c_language/bit_operations/NOT_operation.c
@@ -7,7 +7,7 @@ int main(){
     printf("original a value = %3d (%x)\n",a,a);
     a = ~a ; // 1101 0011
     //------------------wrong
-    // printf("a value of 1's complement = %3d (%x)\n",a,a);
+    printf("a value of 1's complement = %3d (%x)\n",a,a);
     printf("a value of 1's complement = %3d (%x)\n",a&0xff,a&0xff);
     a = ~a ; // 0010 1100
     printf("a value of 2's complement = %3d (%x)\n",a,a);
